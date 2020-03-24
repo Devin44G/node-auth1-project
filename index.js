a331f30d3dc0df1199b7f76f5edfde234024e14b
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-23 20:08:21 -0500

    Added routers and endpoints - MVP

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const session = require('express-session');
+const usersRouter = require('./users/users-router.js');
+const authRouter = require('./auth/auth-router');
+const restricted = require('./auth/restricted-middleware.js');
+
+const sessionConfig = {
+  name: 'masterchief',
+  secret: 'this is a secret . . .',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true
+};
 
 const server = express();
       server.use(express.json());
-      // server.use('/api/users', usersRouter);
+      server.use('/api/users', restricted, usersRouter);
+      server.use("/api/auth", authRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Welcome to the server!</h2>
+  `)
+});
 
 const PORT = process.env.PORT || 5000;
 
